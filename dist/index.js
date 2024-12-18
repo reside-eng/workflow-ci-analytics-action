534608ab0fea1540fceb09cd5c9b8ea885a6d0d9
alexsupp <alex.supplee@sideinc.com>
2024-12-18 11:08:59 -0800

    feat: add required option to all getInput

--- dist/index.js
+++ dist/index.js
@@ -20224,7 +20224,7 @@ exports.colors = [6, 2, 3, 4, 5, 1];
 try {
 	// Optional dependency (as in, doesn't need to be installed, NOT like optionalDependencies in package.json)
 	// eslint-disable-next-line import/no-extraneous-dependencies
-	const supportsColor = __nccwpck_require__(1450);
+	const supportsColor = __nccwpck_require__(75);
 
 	if (supportsColor && (supportsColor.stderr || supportsColor).level >= 2) {
 		exports.colors = [
@@ -29554,22 +29554,6 @@ async function _GoogleToken_requestToken() {
     }
 };
 //# sourceMappingURL=index.js.map
-
-/***/ }),
-
-/***/ 3813:
-/***/ ((module) => {
-
-"use strict";
-
-
-module.exports = (flag, argv = process.argv) => {
-	const prefix = flag.startsWith('-') ? '' : (flag.length === 1 ? '-' : '--');
-	const position = argv.indexOf(prefix + flag);
-	const terminatorPosition = argv.indexOf('--');
-	return position !== -1 && (terminatorPosition === -1 || position < terminatorPosition);
-};
-
 
 /***/ }),
 
@@ -41464,149 +41448,6 @@ module.exports = function stubs(obj, method, cfg, stub) {
     return returnVal
   }
 }
-
-
-/***/ }),
-
-/***/ 1450:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-"use strict";
-
-const os = __nccwpck_require__(857);
-const tty = __nccwpck_require__(2018);
-const hasFlag = __nccwpck_require__(3813);
-
-const {env} = process;
-
-let forceColor;
-if (hasFlag('no-color') ||
-	hasFlag('no-colors') ||
-	hasFlag('color=false') ||
-	hasFlag('color=never')) {
-	forceColor = 0;
-} else if (hasFlag('color') ||
-	hasFlag('colors') ||
-	hasFlag('color=true') ||
-	hasFlag('color=always')) {
-	forceColor = 1;
-}
-
-if ('FORCE_COLOR' in env) {
-	if (env.FORCE_COLOR === 'true') {
-		forceColor = 1;
-	} else if (env.FORCE_COLOR === 'false') {
-		forceColor = 0;
-	} else {
-		forceColor = env.FORCE_COLOR.length === 0 ? 1 : Math.min(parseInt(env.FORCE_COLOR, 10), 3);
-	}
-}
-
-function translateLevel(level) {
-	if (level === 0) {
-		return false;
-	}
-
-	return {
-		level,
-		hasBasic: true,
-		has256: level >= 2,
-		has16m: level >= 3
-	};
-}
-
-function supportsColor(haveStream, streamIsTTY) {
-	if (forceColor === 0) {
-		return 0;
-	}
-
-	if (hasFlag('color=16m') ||
-		hasFlag('color=full') ||
-		hasFlag('color=truecolor')) {
-		return 3;
-	}
-
-	if (hasFlag('color=256')) {
-		return 2;
-	}
-
-	if (haveStream && !streamIsTTY && forceColor === undefined) {
-		return 0;
-	}
-
-	const min = forceColor || 0;
-
-	if (env.TERM === 'dumb') {
-		return min;
-	}
-
-	if (process.platform === 'win32') {
-		// Windows 10 build 10586 is the first Windows release that supports 256 colors.
-		// Windows 10 build 14931 is the first release that supports 16m/TrueColor.
-		const osRelease = os.release().split('.');
-		if (
-			Number(osRelease[0]) >= 10 &&
-			Number(osRelease[2]) >= 10586
-		) {
-			return Number(osRelease[2]) >= 14931 ? 3 : 2;
-		}
-
-		return 1;
-	}
-
-	if ('CI' in env) {
-		if (['TRAVIS', 'CIRCLECI', 'APPVEYOR', 'GITLAB_CI', 'GITHUB_ACTIONS', 'BUILDKITE'].some(sign => sign in env) || env.CI_NAME === 'codeship') {
-			return 1;
-		}
-
-		return min;
-	}
-
-	if ('TEAMCITY_VERSION' in env) {
-		return /^(9\.(0*[1-9]\d*)\.|\d{2,}\.)/.test(env.TEAMCITY_VERSION) ? 1 : 0;
-	}
-
-	if (env.COLORTERM === 'truecolor') {
-		return 3;
-	}
-
-	if ('TERM_PROGRAM' in env) {
-		const version = parseInt((env.TERM_PROGRAM_VERSION || '').split('.')[0], 10);
-
-		switch (env.TERM_PROGRAM) {
-			case 'iTerm.app':
-				return version >= 3 ? 3 : 2;
-			case 'Apple_Terminal':
-				return 2;
-			// No default
-		}
-	}
-
-	if (/-256(color)?$/i.test(env.TERM)) {
-		return 2;
-	}
-
-	if (/^screen|^xterm|^vt100|^vt220|^rxvt|color|ansi|cygwin|linux/i.test(env.TERM)) {
-		return 1;
-	}
-
-	if ('COLORTERM' in env) {
-		return 1;
-	}
-
-	return min;
-}
-
-function getSupportLevel(stream) {
-	const level = supportsColor(stream, stream && stream.isTTY);
-	return translateLevel(level);
-}
-
-module.exports = {
-	supportsColor: getSupportLevel,
-	stdout: translateLevel(supportsColor(true, tty.isatty(1))),
-	stderr: translateLevel(supportsColor(true, tty.isatty(2)))
-};
 
 
 /***/ }),
@@ -68097,20 +67938,22 @@ async function sendToBigQuery(analyticsObject) {
 }
 async function pipeline() {
     core.info('Successfully triggering CI Analytics action');
-    const createdAt = core.getInput(inputs_1.Inputs.CreatedAt);
-    const startedAt = core.getInput(inputs_1.Inputs.StartedAt);
-    const completedAt = core.getInput(inputs_1.Inputs.CompletedAt);
-    const matrixName = core.getInput(inputs_1.Inputs.MatrixName);
-    const matrixValue = core.getInput(inputs_1.Inputs.MatrixValue);
-    const result = core.getInput(inputs_1.Inputs.Result);
-    const draft = core.getInput(inputs_1.Inputs.Draft);
-    const jobLink = core.getInput(inputs_1.Inputs.JobLink);
-    const triggeringActor = core.getInput(inputs_1.Inputs.TriggeringActor);
-    const runAttempt = Number(core.getInput(inputs_1.Inputs.RunAttempt));
-    const headRef = core.getInput(inputs_1.Inputs.HeadRef);
-    const baseRef = core.getInput(inputs_1.Inputs.BaseRef);
-    const runnerType = core.getInput(inputs_1.Inputs.RunnerType);
-    const runnerName = core.getInput(inputs_1.Inputs.RunnerName);
+    const createdAt = core.getInput(inputs_1.Inputs.CreatedAt, { required: true });
+    const startedAt = core.getInput(inputs_1.Inputs.StartedAt, { required: true });
+    const completedAt = core.getInput(inputs_1.Inputs.CompletedAt, { required: true });
+    const matrixName = core.getInput(inputs_1.Inputs.MatrixName, { required: true });
+    const matrixValue = core.getInput(inputs_1.Inputs.MatrixValue, { required: true });
+    const result = core.getInput(inputs_1.Inputs.Result, { required: true });
+    const draft = core.getInput(inputs_1.Inputs.Draft, { required: true });
+    const jobLink = core.getInput(inputs_1.Inputs.JobLink, { required: true });
+    const triggeringActor = core.getInput(inputs_1.Inputs.TriggeringActor, {
+        required: true,
+    });
+    const runAttempt = Number(core.getInput(inputs_1.Inputs.RunAttempt, { required: true }));
+    const headRef = core.getInput(inputs_1.Inputs.HeadRef, { required: true });
+    const baseRef = core.getInput(inputs_1.Inputs.BaseRef, { required: true });
+    const runnerType = core.getInput(inputs_1.Inputs.RunnerType, { required: true });
+    const runnerName = core.getInput(inputs_1.Inputs.RunnerName, { required: true });
     core.info('github context object: ');
     core.info(JSON.stringify(github_1.context, null, 2));
     const { repo: { repo: repository }, workflow, job, actor, runId, runNumber, sha, eventName, } = github_1.context;
@@ -68166,6 +68009,14 @@ async function run() {
     await pipeline().catch(handleError);
     process.removeListener('unhandledRejection', handleError);
 }
+
+
+/***/ }),
+
+/***/ 75:
+/***/ ((module) => {
+
+module.exports = eval("require")("supports-color");
 
 
 /***/ }),
